6f206dd41647488bf8a1710dc90f16aabd304465
dlobregon <troyanrex@gmail.com>
2023-10-17 22:36:51 -0600

    update endpoint port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser')
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
