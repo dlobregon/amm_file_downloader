b287cd8fc53897dc4da7d94900e21c432ef60bc4
dlobregon <troyanrex@gmail.com>
2023-10-18 01:15:06 -0600

    update request

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express();
 const port = 4000;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
