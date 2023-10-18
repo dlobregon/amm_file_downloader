c8bfd6b7f8370b670a91d8453272ae16f4310ef7
dlobregon <troyanrex@gmail.com>
2023-10-17 22:36:03 -0600

    add endpoint which returns data

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser')
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new sqlite3.Database('amm.db');
+
+app.get('/search', (req, res) => {
+  const { startDate, endDate } = req.query;
+  const query = `
+    SELECT * FROM normalized
+    WHERE fecha_hora >= ? AND fecha_hora <= ?
+  `;
+
+  db.all(query, [startDate, endDate], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.json(rows);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
